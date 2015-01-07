0b2d700d25e38d9943568024975de3d4565b8750
Harvey Thompson <harv.thompson@icloud.com>
2015-01-07 11:17:45 -0800

    Added help and init subcommands, which do nothing.

--- dep.py
+++ dep.py
@@ -44,11 +44,25 @@ def verbose(fmt, *a):
         return
     sys.stderr.write(fmt.format(*a))
     sys.stderr.write("\n")
+
+# --------------------------------------------------------------------------------
+# Command: help
+#
+def command_help(args):
+    print "help", args
+
+# --------------------------------------------------------------------------------
+# Command: init
+#
+def command_init(args):
+    print "init", args    
     
 # --------------------------------------------------------------------------------
 # Main
 #
 parser = argparse.ArgumentParser(description="Manages component based dependencies using version control systems (VCS).")
+
+subparsers = parser.add_subparsers(title="command arguments")
 
 parser.add_argument("--version", action="store_true",
                     help="Show version and exit")
@@ -61,9 +75,24 @@ parser.add_argument("-v", "--verbose", action="store_true",
 parser.add_argument("--dry-run", action="store_true",
                     help="Only show what actions and commands would be executed, make no changes")
 
+parser_help = subparsers.add_parser("help",
+                                    help="Show general or specific command help",
+                                    description="Without any arguments display short help for all commands. With a specified <command> argument show more specific help for the given command.")
+parser_help.set_defaults(func=command_help)
+
+parser_init = subparsers.add_parser("init",
+                                    help="Initialise dependency system for this component",
+                                    description="Initialise dependency system for this component.")
+parser_init.set_defaults(func=command_init)
+
+if len(sys.argv) == 1:
+    parser.print_help()
+    exit(0)
+
 args = parser.parse_args()
 if args.version:
     show_version()
     exit(0)
 
+args.func(args)
 exit(0)
