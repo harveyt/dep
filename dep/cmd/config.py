c37b21adb78b56e5a604abf2323f8904ead72f30
Harvey Thompson <harv.thompson@icloud.com>
2015-02-02 14:28:47 -0800

    Add config, passing test-config-work-dir.

--- dep/cmd/config.py
+++ dep/cmd/config.py
@@ -1,24 +1,29 @@
-# --------------------------------------------------------------------------------
-# Command: config
 #
+# Config Command
+# ==============
+#
+# %%LICENSE%%
+#
+from dep import *
+from dep.helpers import *
+
 def command_config(args):
-    if args.work_dir:
+    if opts.args.work_dir:
         path = find_local_work_dir()
         if path is None:
             error("Cannot determine local working directory")
         print path
-    if args.root_work_dir:
+    if opts.args.root_work_dir:
         path = find_root_work_dir()
         if path is None:
             error("Cannot determine root working directory")
         print path
 
-parser_config = subparsers.add_parser("config",
-                                      help="Dependency configuration",
-                                      description="Dependency configuration.")
+parser_config = opts.subparsers.add_parser("config",
+                                           help="Dependency configuration",
+                                           description="Dependency configuration.")
 parser_config.add_argument("--work-dir", action="store_true",
                            help="Show the working directory of this dependency and exit")
 parser_config.add_argument("--root-work-dir", action="store_true",
                            help="Show the working directory of the root dependency and exit")
 parser_config.set_defaults(func=command_config)
-
