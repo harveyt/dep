cabad79184bcc8e44733c91d7c9439492931dddd
Harvey Thompson <harv.thompson@icloud.com>
2016-01-14 12:15:29 -0800

    Added tag command shortcut.

---
+++ dep/cmd/tag.py
@@ -0,0 +1,19 @@
+#
+# Tag Command
+# ==============
+#
+# %%LICENSE%%
+#
+from dep import *
+from dep.helpers import *
+
+def command_tag(args):
+    root = comp.RootComponent()
+    root.foreach_dependency(["git", "tag"] + opts.rest_args, vars(args))
+
+parser_tag = opts.subparsers.add_parser("tag",
+                                      help="Tag each dependency",
+                                      description="Tag each dependency.")
+add_list_arguments(parser_tag)
+parser_tag.set_defaults(func=command_tag)
+opts.allow_rest.append("tag")
